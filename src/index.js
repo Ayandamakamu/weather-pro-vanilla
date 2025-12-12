3bd441d3b6ab949bbb011be1e83a49bff179127d
Ayandamakamu <ayandamakamu1998@gmail.com>
2025-12-12 08:25:47 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,6 +41,27 @@ function refreshWeather(response) {
   `;
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 
 function searchCity(city) {
   let apiKey = "9d2c884b070b3efb5t3adc74bo030ac1";
