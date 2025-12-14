d36eb1cfd093bef1cbb6f14f519405c65f3e0549
Ayandamakamu <ayandamakamu1998@gmail.com>
2025-12-14 16:58:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp * 1000);
-
   let days = [
     "Sunday",
     "Monday",
@@ -15,46 +14,4 @@ function formatDate(timestamp) {
   let hours = String(date.getHours()).padStart(2, "0");
   let minutes = String(date.getMinutes()).padStart(2, "0");
 
-  return `${day} ${hours}:${minutes}`;
-}
-
-function refreshWeather(response) {
-  let data = response.data;
-
-  document.querySelector("#city").innerHTML = data.city;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    data.temperature.current
-  );
-
-document.querySelector("#details").innerHTML = `
-  ${formatDate(data.time)}, ${data.condition.description}<br>
-  Humidity: <strong>${data.temperature.humidity}%</strong>, 
-  Wind: <strong>${Math.round(data.wind.speed * 3.6)}km/h</strong>
-`;
-
-
-
-  document.querySelector("#icon").innerHTML = `
-    <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${data.condition.icon}.png"
-    alt="${data.condition.description}">
-  `;
-}
-
-
-function searchCity(city) {
-  let apiKey = "9d2c884b070b3efb5t3adc74bo030ac1";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(refreshWeather);
-}
-
-
-document
-  .querySelector("#search-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    let city = document.querySelector("#search-form-input").value;
-    searchCity(city);
-  });
-
-
-searchCity("Paris");
+  return `${
